b274aa0fc29d7d8ad2892bbb6e56d98a8fc5ff31
Aleksey <contramoty@gmail.com>
2022-11-02 00:21:38 +0200

    Решение задачи 2 семинар 1

--- Seminar1/Ex2/Program.cs
+++ Seminar1/Ex2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double b = Convert.ToInt32(Console.ReadLine());
+double max = a;
+double min = b;
+if (b > a)
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine($"Большее число: {max}, меньшее число: {min}.");
