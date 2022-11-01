6d2fcea111237bb2d4ca46248c187356539650d8
Aleksey <contramoty@gmail.com>
2022-11-02 00:34:32 +0200

    Решение задачи 4

---
+++ Seminar1/Ex4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+double c = Convert.ToDouble(Console.ReadLine());
+double result;
+if (a > b)
+{
+    if (a > c)
+    {
+        result = a;
+    }
+    else
+    {
+        result = c;
+    }
+}
+else
+{
+    if (b > c)
+    {
+        result = b;
+    }
+    else
+    {
+        result = c;
+    }
+}
+Console.WriteLine($"Максимальное число: {result}.");
