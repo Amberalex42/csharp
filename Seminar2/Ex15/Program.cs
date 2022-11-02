08e0c2a33ef1578c1781a993e1c37d4bf6fe1771
Aleksey <contramoty@gmail.com>
2022-11-02 23:59:33 +0200

    Решение задачи 15

---
+++ Seminar2/Ex15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("выходной");
+}
+else
+{
+    Console.WriteLine("не выходной");
+}
