707a7292fb979d8f505b23bace17851785bba877
Aleksey <contramoty@gmail.com>
2022-11-02 23:55:31 +0200

    Решение задачи 13

---
+++ Seminar2/Ex13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры у числа нет");
+}
+else
+{
+   while (a > 999)
+   {
+    a = a / 10;
+   }
+    Console.WriteLine(a % 10);
+}
