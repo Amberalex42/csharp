30057d69ba068fdc08e9ce0cfab15b95995237fa
Aleksey <contramoty@gmail.com>
2022-11-03 00:21:06 +0200

    Решение задачи 11 (два варианта)

---
+++ Seminar2/Ex11/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string result = "";
+int digit;
+while (a > 0)
+{
+    digit = a % 10;
+    if (a < 10 || a > 99)
+    {
+        result = Convert.ToString(digit) + result;
+    }
+    a = a / 10;
+}
+Console.WriteLine(result);
+
+//Решение через логарифм:
+// int k = Convert.ToInt32(Math.Log10(a)) + 1;
+// int firstPart = Convert.ToInt32(a / Math.Pow(10, k - 1));
+// int secondPart = Convert.ToInt32(a % Math.Pow(10, k - 2));
+// Console.WriteLine($"{firstPart}{secondPart}");
