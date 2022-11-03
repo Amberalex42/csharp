05026441369cda3e573942c0580da8d7b1244af3
Aleksey <contramoty@gmail.com>
2022-11-04 01:43:10 +0200

    Решение задачи 27

---
+++ Seminar4/Ex27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(getSum(a));
+
+int getSum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
