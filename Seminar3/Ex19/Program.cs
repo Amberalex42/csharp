bc27dde42585fa65125bd8c9abd342c074fdb01f
Aleksey <contramoty@gmail.com>
2022-11-04 01:05:27 +0200

    Решение задачи 19

---
+++ Seminar3/Ex19/Program.cs
@@ -0,0 +1,21 @@
+﻿int invert(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result * 10 + num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+string isPalindrome(int a, int b)
+{
+    if (a == b) return "да";
+    else return "нет";
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int aInvert = invert(a);
+Console.WriteLine(isPalindrome(a, aInvert));
