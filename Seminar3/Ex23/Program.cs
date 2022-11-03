8949275ca812113dda4a125e5e8d17ca729adec6
Aleksey <contramoty@gmail.com>
2022-11-04 01:21:55 +0200

    Решение задачи 23

---
+++ Seminar3/Ex23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(getCubes(a));
+
+string getCubes(int num)
+{
+    string result = "";
+    for(int i = 1; i < num; i++)
+    {
+        result += $"{Math.Pow(i, 3)}, ";
+    }
+    result += $"{Math.Pow(num, 3)}";
+    return result;
+}
