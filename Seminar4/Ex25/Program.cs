0b2db6b78588bbce21b729014c1c2902fcde0fda
Aleksey <contramoty@gmail.com>
2022-11-04 01:38:55 +0200

    Решение задачи 25

---
+++ Seminar4/Ex25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(getPower(a, b));
+
+int getPower(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
