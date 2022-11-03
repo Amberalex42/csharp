b8345e5747a7a4711abc3ec3b15a7dd7885c51fd
Aleksey <contramoty@gmail.com>
2022-11-04 01:50:21 +0200

    Решение задачи 29

---
+++ Seminar4/Ex29/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] getArray()
+{
+    int[] result = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива: ");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] myArray = getArray();
+PrintArray(myArray);
