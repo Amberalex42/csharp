0410442e826c90639e73ffb529776b89899fac22
Aleksey <contramoty@gmail.com>
2022-11-04 02:11:05 +0200

    Решение задачи 26

---
+++ Seminar4/Ex26/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(getCount(a));
+
+int getCount(double num)
+{
+    int result = 0;
+    while (num != Convert.ToInt32(num))
+    {
+        num *= 10;
+    }
+    int intNum = Convert.ToInt32(num);
+    while (intNum > 0){
+        result++;
+        intNum /= 10;
+    }
+    return result;
+}
