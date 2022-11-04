22690ef45c8fec0b77abf277903dc2bde2ed7232
Aleksey <contramoty@gmail.com>
2022-11-04 02:18:48 +0200

    Решение задачи 30

---
+++ Seminar4/Ex30/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] getArray()
+{
+    int[] result = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+bool isMore1(int[] array)
+{
+    int total = 0;
+    for(int i = 0; i < 8; i++) total += array[i];
+    if(total > 4) return true;
+    else return false;
+}
+
+int[] myArray = getArray();
+PrintArray(myArray);
+Console.WriteLine(isMore1(myArray));
