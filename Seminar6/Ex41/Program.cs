465c7ccec21c4e73ed6cf098e13672c62d53f0a3
Aleksey <contramoty@gmail.com>
2022-11-06 22:58:59 +0200

    Решение задачи

---
+++ Seminar6/Ex41/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.WriteLine("Сколько чисел будете вводить?");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[m];
+for(int i = 0; i < m; i++)
+{
+    Console.WriteLine($"Введите {i + 1} число: ");
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
