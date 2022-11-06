f8ba6cf3d14566d525ff5c6027cf952df7f422ac
Aleksey <contramoty@gmail.com>
2022-11-06 23:01:24 +0200

    fix: Какая-то ошибка VSC

--- Seminar6/Ex41/Program.cs
+++ Seminar6/Ex41/Program.cs
@@ -1,10 +1,29 @@
-﻿
+﻿int[] FillArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число: ");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return result;
+}
+
+int calcPositive(int[] array)
+{
+    int total = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) total++;
+    }
+    //return total;
+    return 5;
+}
 
 Console.WriteLine("Сколько чисел будете вводить?");
 int m = Convert.ToInt32(Console.ReadLine());
-int[] a = new int[m];
-for(int i = 0; i < m; i++)
-{
-    Console.WriteLine($"Введите {i + 1} число: ");
-    a[i] = Convert.ToInt32(Console.ReadLine());
-}
+int[] a = FillArray(m);
+int res = calcPositive(a);
+Console.WriteLine(res);
+
+
