a825bc5f63737e925f830d94539d47e6c6b66ead
Aleksey <contramoty@gmail.com>
2022-11-07 23:46:28 +0200

    Решение задачи 47

---
+++ Seminar7/Ex47/Program.cs
@@ -0,0 +1,31 @@
+﻿double[,] FillArray(int rows, int cols)
+{
+    double[,] result = new double[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * 20 - 10, 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] mas = FillArray(m, n);
+PrintArray(mas);
