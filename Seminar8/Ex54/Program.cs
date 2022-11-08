6d54222f1271ad4bcc90b111073248c2702e704a
Aleksey <contramoty@gmail.com>
2022-11-08 23:06:49 +0200

    Решение задачи 54

---
+++ Seminar8/Ex54/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] FillArray(int rows, int cols)
+{
+    int[,] result = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result[i, j] = new Random().Next(1, 101);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k ++)
+            {
+                if (array[i, k] > array[i, j]) (array[i, k], array[i, j]) = (array[i, j], array[i, k]);
+            }
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] a = FillArray(m, n);
+PrintArray(a);
+ChangeArray(a);
+Console.WriteLine();
+PrintArray(a);
