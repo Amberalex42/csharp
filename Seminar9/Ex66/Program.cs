ababebd7eb1ba638ac341780880e0c5577b1316d
Aleksey <contramoty@gmail.com>
2022-11-09 23:05:22 +0200

    Решение задачи 66

---
+++ Seminar9/Ex66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int calcSum(int m, int n)
+{
+    if(n == m)
+    {
+        return m;
+    }
+    else
+    {
+        return n + calcSum(m, n - 1);
+    }
+}
+
+Console.WriteLine(calcSum(m, n));
