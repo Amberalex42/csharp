9b9e54ea592552c169b2c4596231d8a6cc1f14c1
Aleksey <contramoty@gmail.com>
2022-11-09 22:54:26 +0200

    Решение задачи 64

---
+++ Seminar9/Ex64/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNum(int n)
+{
+    if(n == 1) Console.Write($"{n}");
+    else Console.Write($"{n}, ");
+    if(n > 1)
+    {
+        PrintNum(n - 1);
+    }
+    
+}
+
+PrintNum(n);
